d2064ebc6562b8a39ed53415a519931a47721cdf
Goklas <christian.goklas.tif24@polban.ac.id>
2025-04-29 14:02:12 +0700

    First Commit

---
+++ nbtreesC.c
@@ -0,0 +1,58 @@
+#include "nbtrees.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void Create_tree(Isi_Tree X, int Jml_Node){
+
+}
+
+boolean IsEmpty (Isi_Tree P){
+
+}
+
+
+/***** Traversal *****/
+void PreOrder (Isi_Tree P){
+
+}
+
+void InOrder (Isi_Tree P){
+
+}
+
+void PostOrder (Isi_Tree P){
+
+}
+
+void Level_order(Isi_Tree X, int Maks_node){
+
+}
+
+void PrintTree (Isi_Tree P){
+
+}
+
+boolean Search (Isi_Tree P, infotype X){
+
+}
+
+/***** Fungsi Lain *****/
+int nbElmt (Isi_Tree P){
+
+}
+
+int nbDaun (Isi_Tree P){
+
+}
+
+int Level (Isi_Tree P, infotype X){
+
+}
+
+int Depth (Isi_Tree P){
+
+}
+
+int Max (infotype Data1, infotype Data2){
+    
+}
